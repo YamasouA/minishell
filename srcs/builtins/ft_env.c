9d5ca13ff1277acb935cca361ae6337a086a4583
lim <lbooks424@gmail.com>
2022-10-06 13:54:51 +0900

    [fix] changed ft_env func to output their own copy of the environment variable list

--- srcs/builtins/ft_env.c
+++ srcs/builtins/ft_env.c
@@ -1,25 +1,28 @@
 #include "minishell.h"
 
-//envp is temporary placement, replace environ later
-void	ft_env(char **strs, char *envp[])
+void	ft_env(char **strs, t_env *envp)
 {
-	int	i;
-
 	if (strs[1])
 	{
-		ft_putstr_fd("env: does not support arguments", 2);
+		ft_putstr_fd("env: does not support arguments\n", 2);
 		return ;
 	}
-	i = 0;
-	while (envp[i])
-	{
-		ft_putstr_fd(envp[i++], 1);
-		ft_putstr_fd("\n", 1);
-	}
+	print_env(envp);
 }
 
-int main(int argc, char **argv, char *envp[])
+int main(void)
 {
 	char	*strs[2] = {"env"};
-	ft_env(strs, envp);
+	t_env *environ;
+
+	environ	= create_env();
+	ft_env(strs, environ);
+	printf("\n\n");
+	{
+	char	*strs[3] = {"env", "abc"};
+
+	environ	= create_env();
+	ft_env(strs, environ);
+	}
+	return (0);
 }
