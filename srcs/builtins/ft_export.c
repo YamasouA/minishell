76bae20e55621dd21ff0640591aa08dd8811f1ec
lim <lbooks424@gmail.com>
2022-10-06 17:51:26 +0900

    [fix] changed ft_export func to update variables or print list from their own copied environment variable list

--- srcs/builtins/ft_export.c
+++ srcs/builtins/ft_export.c
@@ -20,34 +20,37 @@ bool	is_valid_var(char *str)
 }
 
 
-int	update_env_var(char *arg, char *envp[], int j, bool append_flag)
+//int	update_env_var(char *arg, t_env *envp, bool append_flag)
+int	update_env_var(char *arg, t_env *envp, int flag)
 {
-	char	**split_env;
+//	char	**split_env;
 	char	*equal_pos;
 	char	**split_args;
 	char	*value;
+	t_env	*target_var;
+	char	*tmp;
 
 	split_args = ft_split(arg, '='); // must free
 //	i = -1;
 //	while (envp[i++])
 //	{
-	split_env = ft_split(envp[j], '='); //must free
-	if (ft_strlen(split_args[0]) == ft_strlen(split_env[0])
-		&& ft_strncmp(split_args[0], split_env[0], ft_strlen(split_args[0])) == 0)
+//	split_env = ft_split(envp[j], '='); //must free
+//	if (ft_strlen(split_args[0]) == ft_strlen(split_env[0])
+//		&& ft_strncmp(split_args[0], split_env[0], ft_strlen(split_args[0])) == 0)
+	target_var = search_env(envp, split_args[0]);
+	if (target_var)
 	{
-		if (append_flag)
+		equal_pos = ft_strchr(arg, '=');
+		value = ft_substr(equal_pos, 1, ft_strlen(equal_pos + 1));
+		if (flag & 2)
+			target_var->value = ft_joinfree(target_var->value, value);
+		else if (flag & 1)
 		{
-			equal_pos = ft_strchr(arg, '=');
-			value = ft_substr(equal_pos, 1, ft_strlen(equal_pos + 1));
-			envp[j] = ft_strjoin(envp[j], value);
-			free(value);
-			return (1);
+			tmp = target_var->value;
+			target_var->value = value;
+			free(tmp);
 		}
-		else
-		{
-			envp[j] = arg;
-			return (1);
-		}
+		return (1);
 	}
 //	}
 	return (0);
@@ -60,50 +63,99 @@ void	print_invalid_identifier(char *str)
 	ft_putstr_fd("': not a valid identifier\n", 2);
 }
 
-void	add_var_to_env(char **strs, char **envp)
+void	push_new_env_var(char *arg, t_env *envp)
+{
+	t_env	*new;
+
+	new = (t_env *)malloc(sizeof(t_env) * 1);
+	if (new == NULL)
+		return ;
+	else
+		set_data(arg, new);
+	add_env(&envp, new);
+}
+
+int	is_append_flag(char **key, char *eq_pos)
+{
+	char	*tmp;
+
+	if (eq_pos && eq_pos != *key && *(eq_pos - 1) == '+' && *key[0] != '+')
+	{
+		*(eq_pos - 1) = '\0';
+		tmp = *key;
+		*key = ft_strjoin(*key, eq_pos);
+//		free(tmp); //The actual argument(key) is taken in malloc, so it is free.
+		return (1);
+	}
+	return (0);
+}
+
+int	which_update_flag(char **key)
+{
+	char	**split_args;
+	char	*eq_pos;
+	int		flag;
+
+	flag = 0;//define EXPORT_NOME
+	eq_pos = ft_strchr(*key, '=');
+	if (eq_pos)
+	{
+		if (is_append_flag(key, eq_pos))
+			flag = 2; //define EXPORT_APPEND
+		else
+			flag = 1; //define EXPORT_NEW
+	}
+	split_args = ft_split(*key, '=');
+	if (!is_valid_var(split_args[0]))
+	{
+		//free split_args
+		print_invalid_identifier(*key);
+		return (4); //define EXPORT_ERROR
+	}
+	//free split_args
+	return (flag); 
+}
+
+int	add_var_to_env(char **args, t_env *envp)
 {
 	int	i;
-	int	j;
-	char	*equal_pos;
-	bool	append_flag;
-	char	**split_args;
-
+	char	*eq_pos;
+	int	flag;
+	int	exit_status;
+//	bool	append_flag;
+//	char	**split_args;
+	exit_status = 0;
 	i = 0;
-	while (strs[++i])
+	while (args[++i])
 	{
-		append_flag = 0;
-		split_args = ft_split(strs[i], '=');
-		if (!is_valid_var(split_args[0]) || *equal_pos == strs[i][0])
+//		flag = -1;
+//		eq_pos = ft_strchr(args[i], '=');
+		flag = which_update_flag(&args[i]);
+		if (flag == 4)
+			exit_status = 1;
+//		if (is_append_flag(&args[i], eq_pos))
+//			append_flag = 1;
+//		split_args = ft_split(args[i], '=');
+//		if (!is_valid_var(split_args[0]) || *eq_pos == args[i][0])
+//		{
+//			print_invalid_identifier(args[i]);
+//			continue ;	
+//		}
+//		if (eq_pos && is_valid_var(split_args[0]))
+		if (flag & 1 || flag & 2)
 		{
-			print_invalid_identifier(strs[i++]);
-			continue ;	
+//			if (!update_env_var(args[i], envp, append_flag))
+			if (!update_env_var(args[i], envp, flag))
+				push_new_env_var(args[i], envp);
 		}
-		equal_pos = ft_strchr(strs[i], '=');
-		if (equal_pos)
-		{
-			if (*equal_pos != strs[i][0] && *(equal_pos - 1) == '+' && strs[i][0] != '+') // cut is_append_env
-			{
-				*(equal_pos - 1) = '\0';
-				strs[i] = ft_strjoin(strs[i], equal_pos); //free strs[i]
-				append_flag = 1;
-			}
-			j = -1;
-			while (envp[++j])
-			{
-				if (update_env_var(strs[i], envp, j, append_flag))
-					break ;
-			}
-			if (envp[j] == NULL)
-			{
-				envp[j] = strs[i];
-				envp[j + 1] = NULL;
-			}
-		}
+//		else
+//			print_invalid_identifier(args[i]);
 	}
+	return (exit_status);
 }
 
 //add_var_to_env
-//while strs[idx]
+//while args[idx]
 //	split delimi =
 //	if !is_valid_split[0]
 //		print error and set_error_status 1, but continue
@@ -122,41 +174,41 @@ void	add_var_to_env(char **strs, char **envp)
 //			env[i + 1] == NULL
 //	idx++
 
-void	print_prefix_env(char *envp[])
+void	print_prefix_env(t_env *envp)
 {
-	int	i;
-
-	i = 0;
-	while (envp[i])
+	while (envp)
 	{
-		printf("declare -x %s\n", envp[i++]); //after '=' enclose with a double quotation
+		printf("declare -x %s=\"%s\"\n", envp->key, envp->value);
+		envp = envp->next;
 	}
 }
 
-void	ft_export(char **strs, char *envp[])
+int	ft_export(char **args, t_env *envp)
 {
-	if (strs[1])
+	int	exit_status;
+
+	exit_status = 0;
+	if (args[1])
 	{
-		add_var_to_env(strs, envp);
+		exit_status = add_var_to_env(args, envp);
 	}
 	else
 	{
 		print_prefix_env(envp);
 	}
+	return (exit_status);
 }
 
-int	main(int argc, char **argv, char *envp[])
+int	main(int argc, char **argv)
 {
-	int	i;
-
-	i = 0;
-	ft_export(argv, envp);
+	t_env	*env;
+	int	exit_status;
+	
+	env = create_env();
+	exit_status = ft_export(argv, env);
 	if (argv[1])
-	{
-		while (envp[i])
-		{
-			printf("%s\n", envp[i++]);
-		}
-	}
+		print_env(env);
+	printf("\n");
+	printf("exit_status: %d\n", exit_status);
 	return (0);
 }
