676ca0f96362f7af43955b17189649fa1c2f2bac
YamasouA <akiakiskyhand@gmail.com>
2022-10-09 00:31:47 +0900

    [WIP] fix

--- srcs/execution/execute.c
+++ srcs/execution/execute.c
@@ -1,5 +1,42 @@
 #include "minishell.h"
 
+bool	is_redirect(t_cmd *cmd)
+{
+	if (cmd->redirect_in->next == NULL && cmd->redirect_out->next == NULL)
+		return (false);
+	return (true);
+}
+
+bool	which_builtin(char **cmd)
+{
+	static char	*builtins[] = {"cd", "echo", "unset", "export", "exit", "pwd", "env"};
+	size_t	i;
+
+	i = 0;
+	while (builtins[i] != NULL)
+	{
+		if (ft_strncmp(builtins[i], cmd[0], ft_strlen(builtins[i]) + 1))
+			return (true);
+		i++;
+	}
+	return (false);
+}
+
+void	exec_builtin(t_cmd->cmd)
+{
+	static char	*builtins[] = {"cd", "echo", "unset", "export", "exit", "pwd", "env"};
+}
+
+void	exe_process(t_cmd *cmd)
+{
+	if (is_redirect(cmd))
+		do_redirect(cmd);
+	if (is_builtins(cmd->cmd))
+		exec_builtin(cmd);
+	else
+		exec_others(cmd);
+}
+
 void	exe_cmd(t_cmd *cmd)
 {
 	int	fd[2];
@@ -24,17 +61,17 @@ void	exe_cmd(t_cmd *cmd)
 		close(fd[1]);
 		return ;
 	}
-
-
 }
 
-void	exec(t_node *node)
+void	exec(t_node *node, int pipe_flag)
 {
 	if (node->lhs == NULL && node->rhs == NULL)
 	{
-		if (node->cmd->cmd == NULL)
-			return ;
-		if (node->cmd->cmd[0] != NULL)
+		if (!pipe_flag && is_builtins(node->cmd->cmd))
+		{
+			exe_process(node->cmd);
+		}
+		else if (node->cmd->cmd[0] != NULL)
 		{
 			exe_cmd(node->cmd);
 		}
@@ -42,9 +79,9 @@ void	exec(t_node *node)
 	if (node->lhs != NULL || node->rhs != NULL)
 	{
 		if (node->lhs != NULL)
-			exec(node->lhs);
+			exec(node->lhs, 1);
 		if (node->rhs != NULL)
-			exec(node->rhs);
+			exec(node->rhs, 1);
 	}
 	waitpid(-1, &WEXITSTATUS, 0);
 }
