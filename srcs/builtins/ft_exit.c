d7f3d2df975e47ba974948ae17cba9b32c9563cb
YamasouA <akiakiskyhand@gmail.com>
2022-10-29 23:44:16 +0900

    [fix] exit can handle -- pattern

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -1,50 +1,50 @@
 #include "minishell.h"
 
-
-void	exit_with_str(char **strs)
+void	exit_with_str(char **strs, int i)
 {
 	ft_putendl_fd("exit", STDERR_FILENO);
 	ft_putstr_fd("minishell : exit: ", STDERR_FILENO);
-	ft_putstr_fd(strs[1], STDERR_FILENO);
+	ft_putstr_fd(strs[i], STDERR_FILENO);
 	ft_putendl_fd(": numeric argument required", STDERR_FILENO);
 	exit(255);
 }
 
-void	exit_with_number(char **strs, size_t cnt)
+void	exit_with_number(char **strs, size_t cnt, size_t i)
 {
 	long long n;
 	bool	ret;
 
-	if (cnt > 2)
+	if (cnt > i + 1)
 	{
 		ft_putendl_fd("exit", STDOUT_FILENO);
 		ft_putendl_fd("minishell: exit: too many arguments", STDERR_FILENO);
 	}
 	else
 	{
-		ret = ft_atol_bool(strs[1], &n);
+		ret = ft_atol_bool(strs[i], &n);
 		if (ret)
 		{
 			ft_putendl_fd("exit", STDOUT_FILENO);
 			exit(n);
 		}
 		else
-			exit_with_str(strs);
+			exit_with_str(strs, i);
 	}
 }
 
-void	exit_with_arg(char **strs, size_t cnt)
+void	exit_with_arg(char **strs, size_t cnt, size_t idx)
 {
 	bool	is_digit;
 	size_t	i;
 
 	i = 0;
 	is_digit = true;
-	if (strs[1][i] == '-' | strs[1][i] == '+')
+	//if (strs[1][i] == '-' | strs[1][i] == '+')
+	if (strs[idx][i] == '-' | strs[idx][i] == '+')
 		i++;
-	while (strs[1][i])
+	while (strs[idx][i])
 	{
-		if (!ft_isdigit(strs[1][i]))
+		if (!ft_isdigit(strs[idx][i]))
 		{
 			is_digit = false;
 			break;
@@ -52,9 +52,24 @@ void	exit_with_arg(char **strs, size_t cnt)
 		i++;
 	}
 	if (is_digit)
-		exit_with_number(strs, cnt);
+		exit_with_number(strs, cnt, idx);
 	else
-		exit_with_str(strs);
+		exit_with_str(strs, idx);
+}
+
+void	handle_hyphen(char **strs, int cnt)
+{
+	if (cnt == 2)
+	{
+		ft_putendl_fd("exit", STDOUT_FILENO);
+		exit(g_exit_status);
+	}
+	else
+	{
+		if (ft_strncmp(strs[2], "-", ft_strlen(strs[2]) + 1) == 0)
+			exit_with_str(strs, 2);
+		exit_with_arg(strs, cnt, 2);
+	}
 }
 
 int ft_exit(char **strs)//, t_env *envp)
@@ -67,11 +82,14 @@ int ft_exit(char **strs)//, t_env *envp)
 	if (cnt == 1)
 	{
 		ft_putendl_fd("exit", STDOUT_FILENO);
-		exit(0);
+		exit(g_exit_status);
 	}
 	else
 	{
-		exit_with_arg(strs, cnt);
+		if (ft_strncmp("--", strs[1], ft_strlen(strs[1]) + 1) == 0)
+			handle_hyphen(strs, cnt);
+		else
+			exit_with_arg(strs, cnt, 1);
 	}
 	return (1);
 }
