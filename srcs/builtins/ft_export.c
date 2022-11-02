d6317a51446aa43b23a355fea19a5335b92e4ab3
lim <lbooks424@gmail.com>
2022-11-03 00:36:43 +0900

    [add] print builtin usage in export

--- srcs/builtins/ft_export.c
+++ srcs/builtins/ft_export.c
@@ -170,15 +170,32 @@ void	print_prefix_env(t_env *envp)
 	}
 }
 
+int	print_error_and_usage(char *arg, char *cmd_name)
+{
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(cmd_name, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putchar_fd(arg[0], 2);
+	ft_putchar_fd(arg[1], 2);
+	ft_putstr_fd(": options not supported\n", 2);
+	ft_putstr_fd(cmd_name, 2);
+	ft_putstr_fd(": usage: ", 2);
+	ft_putstr_fd(cmd_name, 2);
+	ft_putstr_fd(" [name[=value] ...] or export\n", 2);
+	return (2);
+}
+
 int	ft_export(char **args)
 {
 	int	exit_status;
 
 	exit_status = 0;
-	if (args[1])
+	if (args[1] && !(args[1][0] == '-' && args[1][1]))
 		exit_status = add_var_to_env(args);
-	else
+	else if (args[1] == NULL)
 		print_prefix_env(g_sh_var.environ);
+	else
+		exit_status = print_error_and_usage(args[1], "export");
 	return (exit_status);
 }
 
