ba1adf65082e8b9aa7a2ce948392cea2c3e50109
lim <lbooks424@gmail.com>
2022-11-12 01:16:20 +0900

    [fix] Add malloc error handling

--- srcs/builtins/ft_export.c
+++ srcs/builtins/ft_export.c
@@ -22,17 +22,23 @@ static void	update_target_var_value(char *arg, t_env *target_var, int flag)
 	if (!equal_pos)
 		return ;
 	value = ft_substr(equal_pos, 1, ft_strlen(equal_pos + 1));
-	if (flag & EXPORT_APPEND)
+	if (value == NULL)
+		err_exit("malloc error: ");
+	if (flag == EXPORT_APPEND)
 	{
-		target_var->value = ft_joinfree(target_var->value, value);
+		if (target_var->value)
+			target_var->value = ft_joinfree(target_var->value, value);
+		else
+			target_var->value = ft_strdup(value);
+		if (target_var->value == NULL)
+			err_exit("malloc error: ");
 	}
-	else if (flag & EXPORT_NEW)
+	else if (flag == EXPORT_NEW)
 	{
 		tmp = target_var->value;
 		target_var->value = value;
 		free(tmp);
 	}
-	return ;
 }
 
 static int	update_env_var(char *arg, int flag)
@@ -41,6 +47,8 @@ static int	update_env_var(char *arg, int flag)
 	t_env	*target_var;
 
 	split_args = ft_split(arg, '=');
+	if (split_args == NULL)
+		err_exit("malloc error: ");
 	target_var = search_env(g_sh_var.environ, split_args[0]);
 	free_strs(split_args);
 	if (target_var)
@@ -58,8 +66,7 @@ static void	push_new_env_var(char *arg)
 	new = (t_env *)malloc(sizeof(t_env) * 1);
 	if (new == NULL)
 		err_exit("malloc error: ");
-	else
-		set_data(arg, new);
+	set_data(arg, new);
 	add_env(&g_sh_var.environ, new);
 }
 
@@ -76,7 +83,7 @@ static int	add_var_to_env(char **args)
 		flag = which_update_flag(&args[i]);
 		if (flag == EXPORT_ERROR)
 			exit_status = 1;
-		if (flag & EXPORT_NEW || flag & EXPORT_APPEND)
+		if (flag == EXPORT_NEW || flag == EXPORT_APPEND)
 		{
 			if (!update_env_var(args[i], flag))
 				push_new_env_var(args[i]);
@@ -90,11 +97,11 @@ int	ft_export(char **args)
 	int	exit_status;
 
 	exit_status = 0;
-	if (args[1] && !(args[1][0] == '-' && args[1][1]))
-		exit_status = add_var_to_env(args);
+	if (args[1] && (args[1][0] == '-' && args[1][1]))
+		exit_status = print_error_and_usage(args[1], "export");
 	else if (args[1] == NULL)
 		print_prefix_env(g_sh_var.environ);
 	else
-		exit_status = print_error_and_usage(args[1], "export");
+		exit_status = add_var_to_env(args);
 	return (exit_status);
 }
